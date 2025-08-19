1d3b3da46c9ab7f529c179c9ec79d4fbc58cf97c
kiqrr <caiquerr99@gmail.com>
2025-08-19 20:46:58 -0300

    views

--- receitas/views.py
+++ receitas/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from django.http import Http404
 
 # Create your views here.
 
 def home(request):
     return render(request, 'receitas/home.html')
+
+def receita_detail(request, id):
+    context = {
+        'receita_id': id,
+        'receita_title': f'Receita Detalhada {id}',
+        'receita_description': f'Esta é a descrição detalhada da receita com ID {id}.'
+    }
+
+    return
+    
